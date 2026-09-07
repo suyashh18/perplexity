497c4faeba06e73c5b3dfb803a41a5442422f89a
Suyash Jain <jainsuyash481@gmail.com>
2026-09-07 19:59:25 +0530

    dd

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,29 +1,32 @@
 import jwt from 'jsonwebtoken'
 
 export async function identifyUser(req, res, next) {
+    console.log("COOKIE HEADER:", req.headers.cookie)
+    console.log("PARSED COOKIES:", req.cookies)
+
     const token = req.cookies.token
 
     if (!token) {
-        return res.status(400).json({
-            message: "unauthorized token",
-            success: true,
-            err: "token not present"
+        return res.status(401).json({
+            message: "Unauthorized",
+            success: false,
+            err: "Token not present"
         })
     }
+
     try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.user = decoded;
-
-        next();
+        req.user = decoded
+        next()
 
     } catch (err) {
+        console.log("JWT ERROR:", err)
+
         return res.status(401).json({
             message: "Unauthorized",
             success: false,
             err: "Invalid token"
         })
     }
-
 }
